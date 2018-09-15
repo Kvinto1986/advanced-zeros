ccb6bd1119ac5057b98de157b79734c62998b494
Kvinto1986 <kvinto1986@gmail.com>
2018-09-16 01:45:43 +0300

    crutches :)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+    var zeros_one = 0;
+    var count=base;
+    var tmp = 2;
+    var evr=5;
+if(base%2!=0 || base==16||  base==64){
+    evr=base
 }
+else if(base==Math.pow(2,count/2))
+    evr=base
+       else do {
+            tmp = count / 2;
+            count = tmp
+        if(count%2!=0 && count%3==0 &&count!=3)
+                evr=count/3
+            else evr=tmp
+        }
+        while (count%2==0)
+
+    for(var i=evr;number/i>=1;i=i*evr) {
+        zeros_one = zeros_one + Math.floor(number/ i);
+    }
+    return zeros_one;
+  }
